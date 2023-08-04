46e76a35580ffea72faa8a355e187f18cd7d3ffc
Gabriel02ps <gabriel02ps@gmail.com>
2023-08-04 19:28:53 -0300

    chore(form): add form structure and input fields

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -16,7 +16,7 @@ export function Control(props: InputControlProps) {
   return (
     <input
       type="text"
-      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600 outline-none focus:ring-0"
       {...props}
     />
   )
