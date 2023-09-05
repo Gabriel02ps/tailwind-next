6852c9f61383af7a0110fa6c7af3062f5cbf1ba9
Gabriel02ps <gabriel02ps@gmail.com>
2023-09-05 12:44:23 -0300

    feat(select): add animation on open select

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,24 @@ module.exports = {
       borderWidth: {
         6: '6px',
       },
-
       colors: {
         violet: {
           25: 'fcfaff',
         },
+      },
+      keyframes: {
+        slideDownAndFade: {
+          from: { opacity: 0, transform: 'translateY(-2px)' },
+          to: { opacity: 1, transform: 'translateY(0)' },
+        },
+        // slideUpAndFade: {
+        //   from: { opacity: 1 },
+        //   to: { opacity: 0 },
+        // },
+      },
+      animation: {
+        slideDownAndFade: 'slideDownAndFade 0.4s cubic-bezier(0.16, 1, 0.3, 1)',
+        // slideUpAndFade: 'slideUpAndFade 0.2s ease-out',
       },
     },
   },
