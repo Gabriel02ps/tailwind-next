6fe0fcf46e5ff28e6aa2f7bc7dcd08cd84448b42
Gabriel02ps <gabriel02ps@gmail.com>
2023-09-12 22:49:27 -0300

    feat(sidebar): add dark theme on sidebar component

--- src/components/Sidebar/Profile.tsx
+++ src/components/Sidebar/Profile.tsx
@@ -3,22 +3,22 @@ import { Button } from '../Button'
 
 export function Profile() {
   return (
-    <div className="grid grid-cols-profile items-center gap-3">
+    <div className="flex items-center gap-3">
       <img
         src="https://github.com/Gabriel02ps.png"
         alt=""
         className="h-10 w-10 rounded-full"
       />
       <div className="flex flex-col truncate">
-        <span className="text-sm font-semibold text-zinc-700">
+        <span className="block text-sm font-semibold text-zinc-700 dark:text-zinc-100">
           Gabriel Pereira
         </span>
-        <span className="truncate text-sm text-zinc-500">
+        <span className="block text-sm text-zinc-500 dark:text-zinc-400">
           gabriel02ps@gmail.com
         </span>
       </div>
-      <Button type="button" variant="ghost">
-        <LogOut className="h-5 w-5 text-zinc-500" />
+      <Button variant="ghost" className="ml-auto">
+        <LogOut className="h-5 w-5 text-zinc-500 dark:text-zinc-400" />
       </Button>
     </div>
   )
